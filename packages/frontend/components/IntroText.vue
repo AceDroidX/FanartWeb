0ba9719ef3e15b0c6f2ffc292de9cce80bb8183d
AceDroidX <15519693+AceDroidX@users.noreply.github.com>
2022-04-04 20:18:27 +0800

    docs(frontend): 更改简介

--- packages/frontend/components/IntroText.vue
+++ packages/frontend/components/IntroText.vue
@@ -1,6 +1,9 @@
 <template>
   <p>
-    这里是一些介绍<br />
-    第二行
+    策划：<a href="https://space.bilibili.com/3170686/" target="_blank">雨晴</a>
+    <br />
+    研发：<a href="https://github.com/AceDroidX" target="_blank">ACE</a>
+    <br />
+    本网站会定期获取鲨画动态，经管理员筛选进行展示，有bug反馈等可以b站私信：<a href="https://space.bilibili.com/3170686/" target="_blank">雨晴</a>
   </p>
 </template>
