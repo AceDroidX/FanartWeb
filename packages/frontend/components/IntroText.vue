63371e489d662ce58a6c1844f37d2848e7415506
小伙伴 <1350690718@qq.com>
2022-04-04 20:51:06 +0800

    简介修改

--- packages/frontend/components/IntroText.vue
+++ packages/frontend/components/IntroText.vue
@@ -5,5 +5,7 @@
     研发：<a href="https://github.com/AceDroidX" target="_blank">ACE</a>
     <br />
     本网站会定期获取鲨画动态，经管理员筛选进行展示，有bug反馈等可以b站私信：<a href="https://space.bilibili.com/3170686/" target="_blank">雨晴</a>
+    <br />
+    (域名备案中，先用IP凑合一下)
   </p>
 </template>
