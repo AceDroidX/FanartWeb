7fe2c78f8c005f41cd903db97001c439539a6956
AceDroidX <15519693+AceDroidX@users.noreply.github.com>
2022-04-04 21:25:57 +0800

    feat(frontend): 改网站标题

--- packages/frontend/nuxt.config.js
+++ packages/frontend/nuxt.config.js
@@ -13,8 +13,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - FanartWeb',
-    title: 'FanartWeb',
+    // titleTemplate: '%s - FanartWeb',
+    title: '鲨画',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
